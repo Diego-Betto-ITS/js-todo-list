b0d03d37e8589918f68f410bfb0e49cd43b6db8a
Diego Betto <diego@papion.it>
2026-03-11 15:00:05 +0100

    docs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// creare un array di oggetti che rappresentano le todo
 const todoList = [
     {
         etichetta: 'prima todo',
@@ -11,18 +12,20 @@ const todoList = [
     }
 ];
 
+// recuperare gli elementi del DOM
 const listaTodo = document.getElementById('lista-todo');
 const btnAdd    = document.getElementById('add');
 const inputTodo = document.getElementById('new');
 
-console.log(inputTodo)
-
+// disegnare l'elenco delle todo
 function disegnaElenco() {
+    // svuotare la lista prima di ridisegnarla
     listaTodo.innerText = '';
 
+    // ciclare l'array di oggetti e creare un elemento li per ogni todo
     todoList.forEach(
         function(item){
-            // console.log(item);
+            // creare un elemento li, impostare il testo e aggiungerlo alla lista
             const elementoLi = document.createElement('li');
             elementoLi.innerText = item.etichetta;
             elementoLi.classList.add('todo-item');
@@ -31,20 +34,23 @@ function disegnaElenco() {
     );
 }
 
+// disegnare l'elenco delle todo all'avvio della pagina
 disegnaElenco();
 
 function addItem () {
-    console.log(inputTodo.value);
-
+    // creare un nuovo oggetto todo con i dati dell'input
     const newTodo = {
         etichetta: inputTodo.value,
         stato: false,
         order: 1,
     };
 
+    // aggiungere il nuovo oggetto all'array di todo
     todoList.push(newTodo);
 
+    // ridisegna l'elenco delle todo
     disegnaElenco();
 }
 
+// aggiungere un event listener al pulsante per aggiungere una nuova todo
 btnAdd.addEventListener("click", addItem);
